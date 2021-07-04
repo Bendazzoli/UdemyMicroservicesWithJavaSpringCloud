67b0b610072b244a94de9c107fec569fde2c8d9a
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2021-07-04 17:33:03 -0300

    Config hr-payroll calling hr-worker with RestTemplate

--- hr-payroll/src/main/java/com/udemy/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/udemy/hrpayroll/service/PaymentService.java
@@ -1,13 +1,31 @@
 package com.udemy.hrpayroll.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.udemy.hrpayroll.entity.Payment;
+import com.udemy.hrpayroll.entity.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${hr-worker.host}")
+	private String hrWorkerHost;
+	
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+		Map<String, Long>uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId);
+		
+		Worker worker = restTemplate.getForObject(hrWorkerHost + "/workers/{id}", Worker.class, uriVariables);
+
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
