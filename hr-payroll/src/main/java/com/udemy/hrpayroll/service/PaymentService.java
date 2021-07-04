06cec6edc6b86d280ef3e9360f0964ff5590a2c7
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2021-07-04 17:49:31 -0300

    Feign Client

--- hr-payroll/src/main/java/com/udemy/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/udemy/hrpayroll/service/PaymentService.java
@@ -1,13 +1,9 @@
 package com.udemy.hrpayroll.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.udemy.hrpayroll.client.WorkerClient;
 import com.udemy.hrpayroll.entity.Payment;
 import com.udemy.hrpayroll.entity.Worker;
 
@@ -15,17 +11,12 @@ import com.udemy.hrpayroll.entity.Worker;
 public class PaymentService {
 	
 	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Value("${hr-worker.host}")
-	private String hrWorkerHost;
+	private WorkerClient workerClient;
 	
 	public Payment getPayment(Long workerId, int days) {
-		Map<String, Long>uriVariables = new HashMap<>();
-		uriVariables.put("id", workerId);
 		
-		Worker worker = restTemplate.getForObject(hrWorkerHost + "/workers/{id}", Worker.class, uriVariables);
-
+		Worker worker = workerClient.findById(workerId).getBody();
+		
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
